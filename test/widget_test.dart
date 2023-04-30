7b505ccc06f53c8af8f95150953b4c297a8304a3
KajcsaRenataGabriela <kajcsarenata@gmail.com>
2023-04-30 18:35:55 +0300

    removed api_key from tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:image_generator/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(apiKey: '2h7UzP5II_EW0RISr8CwogsZiouEnlhKLXy7szQrZXY',));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
