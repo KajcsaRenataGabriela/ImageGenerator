af75ebe171d3537efee99c645f994f1f6a002505
KajcsaRenataGabriela <kajcsarenata@gmail.com>
2023-05-03 19:13:51 +0300

    moved .env

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'views/home_page.dart';
 
 void main() async {
-  await dotenv.load(fileName: 'lib/.env');
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
